8b5e7ceba44caf800ff88b4b21329c541d6cf583
richardanewman <rick@richardnewman.dev>
2021-12-26 21:13:17 -0700

    abstracts id generation. closes #23.

--- pet-clinic-data/src/main/java/dev/richardnewman/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/dev/richardnewman/sfgpetclinic/services/map/AbstractMapService.java
@@ -7,7 +7,7 @@ import java.util.*;
 
 public class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -17,8 +17,16 @@ public class AbstractMapService<T extends BaseEntity, ID extends Long> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null.");
+        }
 
         return object;
     }
@@ -29,5 +37,17 @@ public class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        Long nextId = null;
+
+        try {
+            nextId  = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+
+        return nextId;
     }
 }
--- pet-clinic-web/src/main/java/dev/richardnewman/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/dev/richardnewman/sfgpetclinic/bootstrap/DataLoader.java
@@ -27,7 +27,6 @@ public class DataLoader implements CommandLineRunner {
 
     private void loadData() {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Micheal");
         owner1.setLastName("Weston");
 
@@ -36,7 +35,6 @@ public class DataLoader implements CommandLineRunner {
 
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenna");
 
@@ -46,7 +44,6 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
@@ -54,7 +51,6 @@ public class DataLoader implements CommandLineRunner {
 
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
